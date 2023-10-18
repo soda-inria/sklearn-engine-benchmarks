886b9b96697b2f747444a9b72220dc184bf52a22
Franck Charras <29153872+fcharras@users.noreply.github.com>
2023-10-18 16:45:26 +0200

    Always pass numpy inputs to scikit-learn-intelex, rather than usm_ndarray that are not well supported currently

--- benchmarks/pca/solvers/scikit_learn_intelex.py
+++ benchmarks/pca/solvers/scikit_learn_intelex.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 from importlib.metadata import version
 
 from benchopt import BaseSolver, safe_import_context
@@ -6,9 +7,9 @@ from benchopt.stopping_criterion import SingleRunCriterion
 with safe_import_context() as import_ctx:
     # isort: off
     import dpctl
-    import dpctl.tensor as dpt
     import numpy as np
     from sklearnex.decomposition import PCA
+    from sklearnex import config_context
 
     # isort: on
 
@@ -73,14 +74,26 @@ class Solver(BaseSolver):
                 f"power_iteration_normalizer={self.power_iteration_normalizer}"
             )
 
+        # TODO: the overhead of the copy of the data from host to device could be
+        # eliminated if scikit-learn-intelex could just take usm_ndarray objects as
+        # input and directly run compute with the underlying memory buffer. The
+        # documentation at
+        # https://intel.github.io/scikit-learn-intelex/latest/oneapi-gpu.html#device-offloading  # noqa
+        # suggests that it is the intended behavior, however in practice
+        # scikit-learn-intelex currently always perform underlying copies
+        # under the hood no matter what, and sometimes fails at doing so. See e.g.
+        # issue at
+        # https://github.com/intel/scikit-learn-intelex/issues/1534#issuecomment-1766266299  # noqa
+
+        # if self.runtime != "numpy":
+        #     device = device = dpctl.SyclDevice(f"{self.runtime}:{self.device}")
+        #     self.X = dpt.asarray(X, copy=True, device=device)
+        # else:
+        #     self.X = X.copy)()
+
         # Copy the data before running the benchmark to ensure that no unfortunate
         # side effects can happen
-        if self.runtime != "numpy":
-            device = device = dpctl.SyclDevice(f"{self.runtime}:{self.device}")
-            self.X = dpt.asarray(X, copy=True, device=device)
-
-        else:
-            self.X = X.copy()
+        self.X = X.copy()
 
         self.n_components = n_components
         self.tol = tol
@@ -93,17 +106,20 @@ class Solver(BaseSolver):
         if power_iteration_normalizer is None:
             power_iteration_normalizer = "auto"
 
-        estimator = PCA(
-            n_components=self.n_components,
-            copy=True,
-            whiten=False,
-            svd_solver=self.svd_solver,
-            tol=self.tol,
-            iterated_power=self.iterated_power,
-            n_oversamples=self.n_oversamples,
-            power_iteration_normalizer=power_iteration_normalizer,
-            random_state=self.random_state,
-        ).fit(self.X, y=None)
+        with nullcontext() if (self.runtime == "numpy") else config_context(
+            target_offload=f"{self.runtime}:{self.device}"
+        ):
+            estimator = PCA(
+                n_components=self.n_components,
+                copy=True,
+                whiten=False,
+                svd_solver=self.svd_solver,
+                tol=self.tol,
+                iterated_power=self.iterated_power,
+                n_oversamples=self.n_oversamples,
+                power_iteration_normalizer=power_iteration_normalizer,
+                random_state=self.random_state,
+            ).fit(self.X, y=None)
 
         self.explained_variance_ratio_ = estimator.explained_variance_ratio_
 
